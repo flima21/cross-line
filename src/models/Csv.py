8526e15579e327e95ee3e4971200ae8ce7a4ff9b
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-09-16 20:11:00 -0300

    :memo: class to manipulate datas

---
+++ src/models/Csv.py
@@ -0,0 +1,3 @@
+class Csv:
+    def __init__(self) -> None:
+        pass
---
+++ src/models/OpenCv.py
@@ -0,0 +1,7 @@
+import cv2 
+import Person
+
+class OpenCv:
+    
+    def __init__(self, type_object: object):
+        pass 
