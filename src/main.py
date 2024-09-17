2044ad37ac9ff17f901f7658dd2bc1ae2905f5de
Felipe Lima <felipe.pedroso2108@gmail.com>
2024-09-17 20:15:17 -0300

    :memo: add: the principal file with implementation function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-#
+from models.OpenCv import OpenCv
+from models.Person import Person
+
+person_detect = Person()
+opencv = OpenCv(person_detect)
+
+opencv.open_window_thread() # open thread the webcam
+opencv.capture_movements() # capture the movements with video
+opencv.dispose_capture() # close de windows
+opencv.export_movie_generated() # export the video
